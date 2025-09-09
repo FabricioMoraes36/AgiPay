7ecbd7c33912f4e0c29a5d24109de1efec2773fd
Fabricio Moraes <Fabricio36365@outlook.com>
2025-09-09 00:03:40 -0300

    "Feat: Criação do metodo para a busca por email seguindo o padrao do jpa"

--- src/main/java/br/com/AgiaPay/PaymentApi/AgiPay/Repositories/UserRepository.java
+++ src/main/java/br/com/AgiaPay/PaymentApi/AgiPay/Repositories/UserRepository.java
@@ -2,8 +2,11 @@ package br.com.AgiaPay.PaymentApi.AgiPay.Repositories;
 
 import br.com.AgiaPay.PaymentApi.AgiPay.Models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    //Metodo para encontrar o usuario com base no login
+    UserDetails findByEmail(String email);
 }
