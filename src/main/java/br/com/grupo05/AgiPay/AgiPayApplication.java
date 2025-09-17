aac38485345b70b017f68ee70b85ff8e910954b3
Fabricio Moraes <Fabricio36365@outlook.com>
2025-09-17 15:03:25 -0300

    First Commit

---
+++ src/main/java/br/com/grupo05/AgiPay/AgiPayApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupo05.AgiPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiPayApplication.class, args);
+	}
+
+}
