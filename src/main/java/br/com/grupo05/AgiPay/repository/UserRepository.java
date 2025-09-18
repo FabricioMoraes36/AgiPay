9ad477c61e83829e177d6514dac774b90fdde94f
Fabricio Moraes <Fabricio36365@outlook.com>
2025-09-18 16:24:36 -0300

    Fix: Correção da classe UserRepository

--- src/main/java/br/com/grupo05/AgiPay/repository/UserRepository.java
+++ src/main/java/br/com/grupo05/AgiPay/repository/UserRepository.java
@@ -3,5 +3,13 @@ package br.com.grupo05.AgiPay.repository;
 import br.com.grupo05.AgiPay.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserModel, Long> {
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+    //Metodo personalizado para achar o usuario por email
+    UserModel findUserByEmail(String email);
+
+    //Metodo personalizado para achar o usuario por cpf
+    UserModel findUserByCpf(String cpf);
 }
